fa966c6ce67e0797c2e2ec801b091a0902a5e690
Ed Asriyan <asriyan1998@yandex.ru>
2016-04-24 00:03:00 +0300

    Filled main function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
+#include <iostream>
+#include "tests.h"
+
 int main(int argc, char* argv[]){
+	std::cout << test(1, 1000000);
+
+	testToFileForExcel("kek.txt", 0, 1000000, 1000, 10);
+
 
 	return 0;
 }
